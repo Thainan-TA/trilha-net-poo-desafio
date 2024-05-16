b232c8bef64727825dbf9adb3bdada4108c9741d
Thainan-TA <81170048+Thainan-TA@users.noreply.github.com>
2024-05-15 23:13:49 -0300

    Add files via upload

---
+++ trilha-net-poo-desafio/Program.cs
@@ -0,0 +1,17 @@
+﻿using DesafioPOO.Models;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Nokia nokia = new Nokia("71999091918", "Nokia 1020", "09876523", 32);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Iphone iphone = new Iphone("71999091924", "Iphone N24", "123654789", 247);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagran");
+    }
+}
